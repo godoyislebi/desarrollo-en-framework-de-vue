e1041644381ed6f17d4eb8260e6a3c86e8dffbd0
godoyislebi <islebi.pinto.godoy24@gmail.com>
2022-04-06 00:16:02 -0300

    ok

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card class="logo py-4 d-flex justify-center">
+        <NuxtLogo />
+        <VuetifyLogo />
+      </v-card>
+      <v-card>
+        <v-card-title class="headline">
+          Desafío Desarrollo en framework de VUE
+        </v-card-title>
+        <v-card-text>
+          <p>Requisitos:</p>
+          <ul>
+            <li>Debes utilizar un framework de UI.</li>
+            <li>
+              La aplicación DEBE ser renderizada por el servidor (SSR), por lo
+              que se debe utilizar Nuxt.js
+            </li>
+            <li>
+              El layout desarrollado queda a libertad de usted, sin embargo,
+              deben existir al menos 1 vista:
+              <ul>
+                <li>
+                  Vista con las tarjetas de los personajes, con los detalles más
+                  importantes de estos.
+                </li>
+              </ul>
+            </li>
+            <li>La navegación debe ser mediante VueRouter (integrado en Nuxt.js).</li>
+          </ul>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/rickandmorty">
+            Ver resultado del desafío realizado
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ pages/rickandmorty/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="personaje in personajesRick"
+      :key="personaje.id"
+      cols="6"
+      sm="12"
+      md="6"
+    >
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+        <template slot="progress">
+          <v-progress-linear
+            color="deep-purple"
+            height="10"
+            indeterminate
+          ></v-progress-linear>
+        </template>
+
+        <v-img height="250" :src="personaje.imagen"></v-img>
+
+        <v-card-title>{{personaje.nombre}}</v-card-title>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexRickAndMorty',
+  data() {
+    return {
+      personajesRick: [
+        {
+          id: 1,
+          nombre: 'Morty',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+        },
+        {
+          id: 2,
+          nombre: 'Rick',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+        },
+        {
+          id: 3,
+          nombre: 'Summer',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+        },
+        {
+          id: 4,
+          nombre: 'Beth',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+        },
+        {
+          id: 5,
+          nombre: 'Jerry',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
